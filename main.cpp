0b63ce67a22ae000af2853227213fd9b2d2c25a7
Sahil Sharma <shifusahil2811@gmail.com>
2026-06-06 20:34:59 +0530

    Basic mmap, unordered_map based BRC impl

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <algorithm>
+#include <charconv>
+#include <fcntl.h>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <sys/mman.h>
+#include <unordered_map>
+#include <vector>
+
+const std::string DEFAULT_FILE = "measurements.txt";
+
+struct LocationData {
+	float	min;
+	float	max;
+	float	tot;
+	int		count;
+};
+
+class BRC {
+public:
+	BRC(std::string file_name) {
+		_file = file_name;
+	}
+
+	int parse() {
+		// Read file by batched lines
+		int fd = open(_file.c_str(), O_RDONLY);
+		if (fd == -1) {
+			std::cerr << "Failed to open file\n";
+			return -1;
+		}
+		auto size = std::filesystem::file_size(_file);
+		char *file_data = static_cast<char *>(
+				mmap(nullptr, size, PROT_READ, MAP_PRIVATE, fd, 0)
+				);
+		if (!file_data) {
+			std::cerr << "Failed to mmap file\n";
+			return 1;
+		}
+		char *p = file_data;
+		char *e = file_data + size;
+		while (p < e) {
+			// parse city
+			while (*p == '\n')++p;
+			char *st = p;
+			while (p < e && *p != ';')++p;
+			std::string_view city(st, p-st);
+			++p;	// skip ';'
+			// parse temp
+			char *val = p;
+			while (p < e && *p != '\n')++p;
+			std::string_view temp_str(val, p - val);
+			float temp = 0.0f;
+			auto [ptr, ec] = std::from_chars(temp_str.data(), temp_str.data() + temp_str.size(), temp);
+			if (ec ==  std::errc()) {
+				_data[city].tot += temp;
+				_data[city].min = _data[city].min > temp ? temp : _data[city].min;
+				_data[city].max = _data[city].max < temp ? temp : _data[city].max;
+				_data[city].count++;
+			}
+		}
+		return 0;
+	}
+
+	// Dumping values
+	void display_all() {
+		for (auto &[city, location]: _data) {
+			std::cout << "City: " << city << " | Min -> " << location.min << " | Max -> " << location.max << " | Avg -> " << location.tot / location.count <<"\n";
+		}
+	}
+
+	void display_sorted() {
+		std::vector<std::string> locations;
+		locations.reserve(_data.size());
+		for(const auto &[key, _]:_data)
+			locations.emplace_back(key);
+		std::sort(locations.begin(), locations.end());
+		for (auto city: locations) {
+			LocationData location = _data[city];
+			std::cout << "City: " << city << " | Min -> " << location.min << " | Max -> " << location.max << " | Avg -> " << location.tot / location.count <<"\n";
+		}
+	}
+private:
+	std::string _file;
+
+	std::unordered_map<std::string_view, LocationData> _data;
+};
+
+
+int main(int argc, char *argv[]) {
+	int ret_code = 0;
+	std::string name;
+	if (argc >= 2)
+		name = argv[1];
+	if (argc > 2 || (argc==2 && name=="help")) {
+		std::cout<< "USAGE: " << argv[0] << " <measurements_file>\n"; 
+		return ret_code;
+	}
+	if (argc == 1)
+		name = DEFAULT_FILE;
+	// Verify file existence and build from file
+	if (!std::filesystem::exists(name)) {
+		std::cerr<<"File "<<name<<" does not exist. Please re-verify the file path\n";
+		return 1;
+	}
+	// Processing the file content
+	BRC processor(name);
+	processor.parse();
+	//processor.display_all();
+	processor.display_sorted();
+	return ret_code;
+}
